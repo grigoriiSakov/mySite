83bfd07bea7a5daf02296cd6d43672da12433f67
grigoriiSakov <sakov74@ya.ru>
2016-07-27 23:22:16 +0500

    Исправлена ошибка появляющаяся при анимации окна

--- JS/login.js
+++ JS/login.js
@@ -10,8 +10,11 @@ if( $(document).ready(function () {
                 $(".login").html(data);
                 $('.popup,.popup_overlay').fadeIn(400); //показываем всплывающее окно
                 $('.closer,.popup_overlay').click(function(){
-                    $('.popup,.popup_overlay').fadeOut(400); //скрываем всплывающее окно
-                    $('.container').removeClass("popup_overlay").removeAttribute('style');
+                    $('.popup,.popup_overlay').fadeOut(400, function () {//скрываем всплывающее окно
+                        $('.container').removeClass("popup_overlay").fadeIn(100);
+
+                    })
+
 
                 });
 
