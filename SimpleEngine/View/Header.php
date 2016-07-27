b26c54f0d91fcb859c7fc8b20968fdad825e2824
grigoriiSakov <sakov74@ya.ru>
2016-07-27 18:04:14 +0500

    Подключена библиотека Jquery

--- SimpleEngine/View/Header.php
+++ SimpleEngine/View/Header.php
@@ -13,6 +13,7 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="http://<?=$_SERVER['HTTP_HOST'] ?>/style/style.css">
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+       <script type="javascript" src="http://<?=$_SERVER['HTTP_HOST'] ?>/JS/jquery-3.1.0.min.js"></script>
 
 </head>
 <body>
