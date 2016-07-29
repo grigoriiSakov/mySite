275f1b037c137c1d7c12eccbaa8d9dc93870549f
grigoriiSakov <sakov74@ya.ru>
2016-07-29 15:07:24 +0500

    Добавлено возвращение ошибки  при получении не валидной формы регистрации

--- JS/login.js
+++ JS/login.js
@@ -3,28 +3,39 @@
  */
 
 if( $(document).ready(function () {
+        function getWindow() {
+            $('.popup,.popup_overlay').fadeIn(400); //показываем всплывающее окно
+            $('.closer').click(function () {
+                $('.popup,.popup_overlay').fadeOut(400, function () {//скрываем всплывающее окно
+                    $('.container').removeClass("popup_overlay").fadeIn(100);
+                })
 
-        $('#login').click(function () {
-            $.get("/page/login",{} , function (data) {
+            });
+        }
+
+        $('#login').click(function () { // авторизация пользователя
+            $.getJSON("/page/login",{} , function (data) {
                 $('.container').addClass("popup_overlay");
                 $(".login").html(data);
-                $('.popup,.popup_overlay').fadeIn(400); //показываем всплывающее окно
-                $('.closer').click(function(){
-                    $('.popup,.popup_overlay').fadeOut(400, function () {//скрываем всплывающее окно
-                        $('.container').removeClass("popup_overlay").fadeIn(100);
-
-                    })
-
-
-                });
-
-
+                getWindow();
             })
         })
-        $(document).on('click','#registration',function () {
-            $.get('/page/registration', {}, function (data) {
-             alert(data)   ;
-            })
+        $(document).on('click','#registration',function () { // регистрация пользователя
+            $.getJSON('/page/registration', {}, function (data) {
+                $(".login").html(data);
+                getWindow();                
+            })           
         })
+        
+        $(document).on('click','#submit_registration',function () { // регистрация пользователя
+            $.post('/page/addRegistration', {}, function (data) {
+                alert(json(data));
+                //$(".login").html(data);
+                //getWindow();
+
+            })
+
+        }) 
+        
     }));
 
